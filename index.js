8c7657d217cf7727e60ce9eab5c752c39d9a375e
rakibhasansabbir <rakibhasansabbir@yahoo.com>
2020-07-18 02:23:28 +0600

    Initiate package

---
+++ index.js
@@ -0,0 +1,26 @@
+exports.parse = function(error) {
+  const errors = []
+  try {
+    const errorObj = error.response.data.errors
+    const result = Object.entries(errorObj)
+    result.forEach((item1) => {
+      item1.forEach((item2, index2) => {
+        if (index2 === 1) {
+          item2.forEach((item3) => {
+            errors.push(item3)
+          })
+        }
+      })
+    })
+  } catch (e) {
+    let message = error.message
+    try {
+      message = error.response.data.message
+    } catch (e) {
+      message = error.message
+    }
+    errors.push(message)
+  }
+  return errors
+}
+  
